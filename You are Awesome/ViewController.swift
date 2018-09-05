2085f56a988d2988c8918375c19e5791a23a325a
BC Swift Student Loan 2 <bcswiftstudentloan2@csomswiftloan2.local>
2018-09-05 15:24:37 -0400

    Initial Commit

---
+++ You are Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You are Awesome
+//
+//  Created by BC Swift Student Loan 2 on 9/5/18.
+//  Copyright © 2018 BC Swift Student Loan 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
