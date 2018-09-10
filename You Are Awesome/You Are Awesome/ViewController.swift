452c1327c827b9724540f9c75746f21c0bf9f922
jacquelinehan1 <42777269+jacquelinehan1@users.noreply.github.com>
2018-09-09 21:58:36 -0400

    Add files via upload

---
+++ You Are Awesome/You Are Awesome/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Jacqueline Han on 9/9/18.
+//  Copyright © 2018 Jacqueline Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        if messageLabel.text == "You Are Awesome!" {
+            messageLabel.text = "You Are Great!"
+        } else if messageLabel.text == "You Are Great!" {
+            messageLabel.text = "You Are Amazing!"
+        } else {
+            messageLabel.text = "You Are Awesome!"
+        }
+    }
+    
+
+    
+}
+
